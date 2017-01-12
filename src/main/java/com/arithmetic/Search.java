4a7f5c05debf2e2b7e25ea2b5452ecee92a31836
DHzhu <longhunguoshang@hotmail.com>
2017-01-12 16:23:56 +0800

    push codes

--- src/main/java/com/arithmetic/Search.java
+++ src/main/java/com/arithmetic/Search.java
@@ -1,7 +1,7 @@
 package com.arithmetic;
 
 /**
- * @desc  : TODO
+ * @desc  : 常用查找方法实现
  * @author: Zhu
  * @date  : 2017年1月11日
  */
@@ -13,17 +13,15 @@ public class Search {
 		
 		int location = -1;
 		
-		location = Search.binarySearch(arr.clone(), 34);
+		//location = Search.binarySearch(arr.clone(), 34);
+		location = Search.binarySearch(arr.clone(), 34, 0, arr.length - 1);
 		
 		System.out.println(location + 1);
 	}
 	
 	/**
-	 * @desc  : 二分查找
-	 * @date  : 2017年1月11日
-	 * @param arr
-	 * @param des
-	 * @return
+	 * @desc : 二分查找
+	 * @date : 2017年1月11日
 	 */
 	public static int binarySearch (int arr[], int des){
 		int low = 0;
@@ -42,5 +40,125 @@ public class Search {
 		
 		return -1;
 	}
-
+	
+	/**
+	 * @desc : 二分查找
+	 * @date : 2017年1月11日
+	 */
+	public static int binarySearch(int arr[], int des, int begin, int end){
+		
+		int middle = (begin + end) / 2;
+		if(des > arr[end] || des < arr[begin] || arr[end] < arr[begin]){
+			return -1;
+		}
+		
+		if(des < arr[middle]){
+			return binarySearch(arr, des, begin, middle - 1);
+		}else if(des > arr[middle]){
+			return binarySearch(arr, des, middle + 1, end);
+		}else{
+			return middle;
+		}
+	}
 }
+
+/**
+ * @desc  : 定义二叉树
+ * @author: Zhu
+ * @date  : 2017年1月12日
+ */
+class BinaryNode{
+	int value;
+	BinaryNode left;
+	BinaryNode right;
+	
+	public BinaryNode(int value){
+		this.value = value;
+		this.left = null;
+		this.right = null;
+	}
+	
+	/**
+	 * @desc : 添加节点
+	 * @date : 2017年1月12日
+	 */
+	public void add(int value){
+		if(value > this.value){
+			if(this.right != null){
+				this.right.add(value);
+			}else{
+				this.right = new BinaryNode(value);
+			}
+		}else{
+			if(this.left != null){
+				this.left.add(value);
+			}else{
+				this.left = new BinaryNode(value);
+			}
+		}
+	}
+	
+	/**
+	 * @desc : 中序查找
+	 * @date : 2017年1月12日
+	 */
+	public BinaryNode midGet(int value){
+		if(this.value == value){
+			return this;
+		}
+		
+		if(value > this.value){
+			return this.right.midGet(value);
+		}
+		
+		if(value < this.value){
+			return this.left.midGet(value);
+		}
+		
+		return null;
+	}
+	
+	/**
+	 * @desc : 先序查找
+	 * @date : 2017年1月12日
+	 */
+	public BinaryNode preGet(int value){
+		
+		return null;
+	}
+	
+	/**
+	 * @desc : 后序查找
+	 * @date : 2017年1月12日
+	 */
+	public BinaryNode postGet(int value){
+		
+		return null;
+	}
+	
+	/**
+	 * @desc : 中序遍历
+	 * @date : 2017年1月12日
+	 */
+	public void midOrder(BinaryNode bn){
+		
+	}
+	
+	/**
+	 * @desc : 先序遍历
+	 * @date : 2017年1月12日
+	 */
+	public void preOrder(BinaryNode bn){
+		
+	}
+	
+	/**
+	 * @desc : 后序遍历
+	 * @date : 2017年1月12日
+	 */
+	public void postOrder(BinaryNode bn){
+		
+	}
+}
+
+
